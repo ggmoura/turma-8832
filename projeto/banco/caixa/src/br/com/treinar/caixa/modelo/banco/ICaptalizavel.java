0f7408b5d647c3fa4fe0646595509c1262d54a0d
ADM <ADM@SL09PROFESSOR>
2016-10-18 21:30:18 -0200

    separacao da camada de modelo

--- projeto/banco/caixa/src/br/com/treinar/caixa/modelo/banco/ICaptalizavel.java
+++
@@ -1,9 +0,0 @@
-package br.com.treinar.caixa.modelo.banco;
-
-public interface ICaptalizavel {
-
-	Integer CICLO = 30;
-	
-	void captalizar();
-	
-}
--- projeto/banco/caixa/src/br/com/treinar/caixa/modelo/banco/ITarifavel.java
+++
@@ -1,7 +0,0 @@
-package br.com.treinar.caixa.modelo.banco;
-
-public interface ITarifavel {
-
-	void tarifar();
-	
-}
