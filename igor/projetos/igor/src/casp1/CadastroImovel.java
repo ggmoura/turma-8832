652b9e676a25ece7e69e1aa02d0a458a1885911c
ADM <ADM@SL09MICRO52>
2016-10-18 20:28:36 -0200

    Signed-off-by: ADM <ADM@SL09MICRO52>

--- igor/projetos/igor/src/casp1/CadastroImovel.java
+++ igor/projetos/igor/src/casp1/CadastroImovel.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class CadastroImovel {
 
 	public static void main(String[] args) {
+		System.out.print("Informe qual ordinal do imóvel: \t");
 		Scanner teclado = new Scanner(System.in);
 		Integer tipoImovel = teclado.nextInt();
 		teclado.close();
 		TipoImovel[] tipoImoveis = TipoImovel.values();
-		System.out.println("O imovel selecionado foi: " + "\t" + tipoImoveis[tipoImovel]);
+		System.out.println("O imóvel informado foi: " + "\t\t" + tipoImoveis[tipoImovel]);
 
 	}
 
