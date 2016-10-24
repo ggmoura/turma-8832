ec9d6790c73e01d4dc93dafbd76900325020fe53
ADM <ADM@SL09PROFESSOR>
2016-10-24 21:50:40 -0200

    Exceptions customizadas

--- projeto/banco/caixa-modelo/src/br/com/treinar/caixa/modelo/ContaSalario.java
+++ projeto/banco/caixa-modelo/src/br/com/treinar/caixa/modelo/ContaSalario.java
@@ -1,16 +1,16 @@
 package br.com.treinar.caixa.modelo;
 
 import br.com.treinar.caixa.modelo.banco.Conta;
+import br.com.treinar.caixa.modelo.exception.SaldoInsuficienteException;
 
 public class ContaSalario extends Conta {
 
-	public Boolean sacar(Double valor) {
-		Boolean sacou = Boolean.FALSE;
+	public void sacar(Double valor) throws SaldoInsuficienteException {
 		if (valor <= saldo) {
 			saldo = saldo - valor;
-			sacou = Boolean.TRUE;
+		} else {
+			throw new SaldoInsuficienteException(recuperarSaldo());
 		}
-		return sacou;
 	}
 	
 }
