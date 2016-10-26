419c4332a96fdaf24b731436703079e2855ad90e
ADM <ADM@SL09PROFESSOR>
2016-10-25 22:29:45 -0200

    Atividade arquivo

--- gleidson/projetos/treinar/src/br/com/treinar/estudo/io/AtividadeLerNomes.java
+++ gleidson/projetos/treinar/src/br/com/treinar/estudo/io/AtividadeLerNomes.java
@@ -1,5 +1,15 @@
 package br.com.treinar.estudo.io;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,29 +21,54 @@ public class AtividadeLerNomes {
 		for (String nome : nomes) {
 			System.out.println(nome);
 		}
+		System.out.println("\n");
 		String nome = "";
 		do {
-			System.out.print("\n\nInforme o nome ou escreva [sair] para finalizar: ");
+			System.out.print("Informe o nome ou escreva [sair] para finalizar: ");
 			nome = leitor.nextLine();
 			if (!nome.equalsIgnoreCase("sair")) {
-				nomes.add(nome);
+				nomes.add(nome.toUpperCase());
 			}
-			leitor.close();
-		} while (nome.equalsIgnoreCase("sair"));
+		} while (!nome.equalsIgnoreCase("sair"));
+		leitor.close();
 		gravarArquivoOrdenado(nomes);
 		System.out.println("fim do programa!");
 	}
 	
-	
 	public static List<String> lerArquivo() {
-		
-		
-		return null;
+		List<String> nomes = new ArrayList<String>();
+		try {
+			InputStream is = new FileInputStream("nomes.trn");
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			System.out.println(br.readLine());
+			
+			String nome = br.readLine(); // primeira linha
+			while (nome != null) {
+				nomes.add(nome);
+				nome = br.readLine();
+			}
+			br.close();
+			
+		} catch (Exception e) {
+			System.out.println("Arquivo não encontrado...");
+		}
+		return nomes;
 	}
 	
-	public static List<String> gravarArquivoOrdenado(List<String> nomes) {
-		
-		
-		return null;
+	public static void gravarArquivoOrdenado(List<String> nomes) {
+		Collections.sort(nomes);
+		try {
+			OutputStream os = new FileOutputStream("nomes.trn");
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			BufferedWriter bw = new BufferedWriter(osw);
+			for (String nome : nomes) {
+				bw.write(nome);
+				bw.newLine();
+			}
+			bw.close();
+		} catch (Exception e) {
+			System.out.println("Erro ao gravar o arquivo...");
+		}
 	}
 }
