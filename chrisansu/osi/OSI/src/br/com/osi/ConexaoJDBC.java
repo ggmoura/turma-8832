19671956a67943ed0316ca3d2f0e024f91135333
Chrisansu <chrisansu@hotmail.com>
2016-10-26 22:43:39 -0200

    conecao

---
+++ chrisansu/osi/OSI/src/br/com/osi/ConexaoJDBC.java
@@ -0,0 +1,35 @@
+package br.com.osi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoJDBC {
+
+	private static ConexaoJDBC instance;
+
+	private ConexaoJDBC() {
+		super();
+	}
+
+	static {
+		instance = new ConexaoJDBC();
+	}
+
+	public static ConexaoJDBC getInstance() {
+		return instance;
+	}
+
+	public Connection getConnection() {
+		try {
+			// conexao com banco de dados online - nao informei a porta, deixei
+			// a padrao 3306
+			return DriverManager.getConnection("jdbc:mysql://mysql.chrisansu.com.br/chrisansu16", "chrisansu16",
+					"adm7treinar");
+
+		} catch (SQLException e) {
+			// se a conexao falhar, mando uma mensagem para o usuário
+			throw new RuntimeException(e);
+		}
+	}
+}
